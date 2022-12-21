1bb67b1e0cc687d965a6ac4a966c499b6575bbef
Abdallah <abdallah.moh.153@gmail.com>
2022-12-22 00:29:59 +0200

    write the schema of product list

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(morgan('dev'));
 //router
 
 
-app.use('/api/v1/categories',categoryRoute )
+app.use('/api/categories',categoryRoute )
 
 //handel route erros 
 app.use('*',(req,res,next) => {
---
+++ models/product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose') ;
+
+const productSchema = mongoose.Schema({
+    title :{
+       type:String ,
+       required:true ,
+       minlength:[3,"this too short"],
+       maxlength:[100,"this too long"]
+    } ,
+    describtion:{
+     type:String ,
+     required:[true,"describtion of product required"] ,
+     minlength:[50,"this too short"]
+    },
+    price:{
+        type:number ,
+        required:[true,"price of product required"],
+        maxlength:[15,"too long"]
+    },
+    quantity:{
+    type :number ,
+    required:[true,"quantity of product required"]
+    },
+    sold:{
+    type:number,
+    default:0
+    },
+    image:{
+        type:String,
+        required:[true ,"product image required"]
+    },
+    category:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Category' ,
+        required:[true,"Category Of Prouct Required"]
+    }
+},{timestamps :true})
+module.exports=mongoose.model('Product',productSchema) ;
