a8798624952008c24dbe8b8df78ebf82e7b9a7b2
Zyad <zyadyasser58@gmail.com>
2022-12-23 23:20:16 +0200

    create file productValidator.js and check the terms for creating product

---
+++ validator/productValidator.js
@@ -0,0 +1,39 @@
+const { check } = require('express-validator')
+const validatorMiddelware = require('../middlewares/validatormiddelware')
+exports.createProductValidation = [
+    check('title')
+        .isLength({ min: 3 })
+        .withMessage('it is to short')
+        .notEmpty()
+        .withMessage('product required'),
+    check('describtion')
+        .notEmpty()
+        .withMessage('product describtion required')
+        .isLength({ max: 2000 })
+        .withMessage('it is to long'),
+    check('price')
+        .notEmpty()
+        .withMessage('product price required')
+        .isNumeric()
+        .withMessage('price must be number')
+        .isLength({ max: 30 })
+        .withMessage('to long price'),
+    check('quantity')
+        .notEmpty()
+        .withMessage('product quantity required')
+        .isNumeric()
+        .withMessage('quantity must be number'),
+    check('sold')
+        .optional()
+        .isNumeric()
+        .withMessage('must be number'),
+    check('image')
+        .notEmpty()
+        .withMessage('image is required'),
+    check('category')
+        .notEmpty()
+        .withMessage('product must be belonges to category')
+        .isMongoId()
+        .withMessage('invaled id formate'),
+    validatorMiddelware,
+];
