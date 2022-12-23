e662cc872c1b69e6ecd07012b00cc174d45ab248
Abdallah <abdallah.moh.153@gmail.com>
2022-12-23 16:24:09 +0200

    orgnize file

--- middlewares/errormiddelware.js
+++ middlewares/errormiddelware.js
@@ -2,6 +2,14 @@
 const ErrormiddelWare=(err,req,res,next) => {
     err.statusCode= err.statusCode ||500 ;
     err.status=err.status||'error';
+if(process.env.Node_Env=="development"){
+sendEroorforDevelopment(err,res);
+}else{
+    sendEroorforproduction(err,res);
+}
+} ;
+
+sendEroorforDevelopment=(err,res) => {
     res.status(err.statusCode).json({
         status:err.status , // for error status
         error:err ,
@@ -9,4 +17,15 @@ const ErrormiddelWare=(err,req,res,next) => {
         stack:err.stack // display place of error
     });
 }
+
+sendEroorforproduction=(err,res) => {
+    res.status(err.statusCode).json({
+        status:err.status , 
+        message:err.message
+    });
+}
+
+
+
+
 module.exports=ErrormiddelWare ;
