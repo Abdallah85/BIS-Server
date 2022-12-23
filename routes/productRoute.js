b8a52fc5581556619fc8afff6f4f2ecba9450145
Zyad <zyadyasser58@gmail.com>
2022-12-23 23:30:40 +0200

    update values for productroute.js file

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,15 @@
-const express =require('express');
-const router =express.Router();
-const {getProducts , getProduct , createProduct} =require('../services/productservices');
-router.route('/').get(getProducts).post(createProduct);
-router.route('/:id').get(getProduct)
-module.exports =router
+const express = require('express');
+const { getProductValidator, createProductValidation, updateProductValidator, deleteProductValidator } = require('../validator/productValidator')
+const { getProducts, getProduct, createProduct, updateProduct, deleteproduct } = require('../services/productservices');
+const router = express.Router();
+router
+    .route('/')
+    .get(getProducts)
+    .post(createProductValidation,createProduct);
+router
+    .route('/:id')
+    .get(getProduct, getProductValidator)
+    .put(updateProduct,updateProductValidator)
+    .delete(deleteproduct,deleteProductValidator);
+
+    module.exports = router;
