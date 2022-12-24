3ebed9d91082119e8f5b88100b21a2c5f8084dc3
Mohamed-Khonany <MKhonany777@gmail.com>
2022-12-24 18:40:25 +0200

    handling errors of categoryVaildator

--- validator/categoryValidator.js
+++ validator/categoryValidator.js
@@ -1,24 +1,23 @@
 const slugify = require('slugify');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const validatormiddelware =require('../middlewares/validatorMiddleware')
 exports.getcategoryvalidation=[
 check('id').isMongoId().withMessage("Invaild Category Id"),
 validatormiddelware 
-];
+]
 
 exports.createCategoryvalidation=[
 check('name').notEmpty().withMessage("Category requried").
 isLength({min:3}).withMessage("Too Short For Category Name").
-isLength({max:40}).withMessage("Too Long For Category Name")
-.custom((val, { req }) => {
- req.body.slug = slugify(val);
- return true;
+isLength({max:40}).withMessage("Too Long For Category Name").custom((val, { req }) => {
+    req.body.slug = slugify(val);
+    return true;
 }),
-validatormiddelware
+validatormiddelware,
 ]
 exports.updatecategoryvalidation=[
 check('id').isMongoId().withMessage("Invaild Category Id"),
-body('name').optional().custom((val, { req }) =>{
+body('name').optional().custom((val, { req }) => {
     req.body.slug = slugify(val);
     return true;
 }),
