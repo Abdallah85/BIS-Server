3226a44e4431cf2e60163579a4e5e556d83821a1
Mohamed-Khonany <MKhonany777@gmail.com>
2022-12-24 18:32:09 +0200

    add slugify in categoryValidator

--- validator/categoryValidator.js
+++ validator/categoryValidator.js
@@ -1,21 +1,30 @@
+const slugify = require('slugify');
 const { check } = require('express-validator');
 const validatormiddelware =require('../middlewares/validatorMiddleware')
 exports.getcategoryvalidation=[
 check('id').isMongoId().withMessage("Invaild Category Id"),
 validatormiddelware 
-]
+];
 
 exports.createCategoryvalidation=[
 check('name').notEmpty().withMessage("Category requried").
 isLength({min:3}).withMessage("Too Short For Category Name").
-isLength({max:40}).withMessage("Too Long For Category Name") ,
+isLength({max:40}).withMessage("Too Long For Category Name")
+.custom((val, { req }) => {
+ req.body.slug = slugify(val);
+ return true;
+}),
 validatormiddelware
 ]
 exports.updatecategoryvalidation=[
 check('id').isMongoId().withMessage("Invaild Category Id"),
-validatormiddelware 
-]
+body('name').optional().custom((val, { req }) =>{
+    req.body.slug = slugify(val);
+    return true;
+}),
+validatormiddelware, 
+];
 exports.deletecategoryvalidation=[
-check('id').isMongoId().withMessage("Invaild Category Id"),
+check('id').isMongoId().withMessage("Invaild Category Id format"),
 validatormiddelware 
 ]
