33e7522c26e5eb36845c4052d6ee5366f6e886e4
Mohamed-Khonany <MKhonany777@gmail.com>
2022-12-24 20:16:02 +0200

    export category & products from factory

--- services/categoryservices.js
+++ services/categoryservices.js
@@ -1,68 +1,99 @@
 const CategoryModel =require('../models/categorymodel')
-const slugify = require('slugify')
-const asyncHandler = require('express-async-handler')
-const ApiError =require('../utils/ApiErrors')
+const asyncHandler = require('express-async-handler');
+const sharp = require('sharp');
+const { v4: uuidv4 } = require('uuid');
+const factory = require('./handlersFactory');
+const { uploadSingleImage } = require('../middlewares/uploadImageMiddleware');
 
-// get method to res all categories in database
-exports.getCategories = asyncHandler(async (req,res) => {
-const page =req.query.page * 1 || 1 ;
-const limit =req.query.limit * 1 || 5 ;
-const skip =(page-1)*limit;
-const allCategories = await CategoryModel.find({}).skip(skip).limit(limit) ;
-res.status(201).json({ result : allCategories.length , data:allCategories})  
-})
+//upload single image
+exports.uploadCategoryImage = uploadSingleImage('image');
 
-
-
-// post method to create category (Only Adamin)
-exports.createCategory =asyncHandler(  async (req ,res) => {
-const name =req.body.name ;
-const category =await CategoryModel.create({name , slug:slugify(name)}) ;
-res.status(201).json({data : category}) ;
-} )
-
-
-// get method to get category by id (specific category)
-exports.getcategory=asyncHandler( async (req , res ,next) => {
-const { id } = req.params ;
-const category =await CategoryModel.findById(id);
-if(!category){
- return next(new ApiError(`No category By This Id ${id}`,404))
-}
-res.status(201).json({data:category}) ;
-})
-
-
-
-
-
-
-//update for category by id (Admin)
-exports.updatecategory= asyncHandler( async(req,res) => {
-const { id } =req.params ;
-const { name } =req.body ;
-const category =await CategoryModel.findByIdAndUpdate({_id:id},{name , slug:slugify(name) },{new:true})
-if(!category){
-    return next(new ApiError(`No category By This Id ${id}`,404))  
+//Image processing
+exports.resizeImage = asyncHandler(async (req, res, next) => {
+    const filename = `category-${uuidv4()}-${Date.now()}.jpeg`;
+  
+    if (req.file) {
+      await sharp(req.file.buffer)
+        .resize(600, 600)
+        .toFormat('jpeg')
+        .jpeg({ quality: 95 })
+        .toFile(`uploads/categories/${filename}`);
+  
+      // Save image into our db
+      req.body.image = filename;
     }
-    res.status(201).json({data:category})
+  
+    next();
+  });
 
-})
+
+// @desc    Get list of categories
+// @route   GET /api/v1/categories
+// @access  Public  
+exports.getCategories = factory.getAll(CategoryModel);
+// exports.getCategories = asyncHandler(async (req,res) => {
+// const page =req.query.page * 1 || 1 ;
+// const limit =req.query.limit * 1 || 5 ;
+// const skip =(page-1)*limit;
+// const allCategories = await CategoryModel.find({}).skip(skip).limit(limit) ;
+// res.status(201).json({ result : allCategories.length , data:allCategories})  
+// })
 
 
-//delete a category by id (Admin)
-exports.deletecategory=asyncHandler( async(req,res) => {
-    const { id } =req.params ;
-    const category =await CategoryModel.findByIdAndDelete(id);
-    if(!category){
-        return next(new ApiError(`No category By This Id ${id}`,404))  
-        }
-        res.status(204).json()
+
+// @desc    Create category
+// @route   POST  /api/v1/categories
+// @access  Private/Admin-Manager
+exports.createCategory = factory.createOne(CategoryModel);
+// exports.createCategory =asyncHandler(  async (req ,res) => {
+// const name =req.body.name ;
+// const category =await CategoryModel.create({name , slug:slugify(name)}) ;
+// res.status(201).json({data : category}) ;
+// } )
+
+
+
+// @desc    Get specific category by id
+// @route   GET /api/v1/categories/:id
+// @access  Public
+exports.getcategory = factory.getOne(CategoryModel);
+// exports.getcategory=asyncHandler( async (req , res ,next) => {
+// const { id } = req.params ;
+// const category =await CategoryModel.findById(id);
+// if(!category){
+//  return next(new ApiError(`No category By This Id ${id}`,404))
+// }
+// res.status(201).json({data:category}) ;
+// })
+
+
+
+// @desc    Update specific category
+// @route   PUT /api/v1/categories/:id
+// @access  Private/Admin-Manager
+exports.updatecategory = factory.updateOne(CategoryModel);
+// exports.updatecategory= asyncHandler( async(req,res) => {
+// const { id } =req.params ;
+// const { name } =req.body ;
+// const category =await CategoryModel.findByIdAndUpdate({_id:id},{name , slug:slugify(name) },{new:true})
+// if(!category){
+//     return next(new ApiError(`No category By This Id ${id}`,404))  
+//     }
+//     res.status(201).json({data:category})
+// })
+
+
+// @desc    Delete specific category
+// @route   DELETE /api/v1/categories/:id
+// @access  Private/Admin
+exports.deletecategory = factory.deleteOne(CategoryModel);
+// exports.deletecategory=asyncHandler( async(req,res) => {
+//     const { id } =req.params ;
+//     const category =await CategoryModel.findByIdAndDelete(id);
+//     if(!category){
+//         return next(new ApiError(`No category By This Id ${id}`,404))  
+//         }
+//         res.status(204).json()
     
-    })
-
-
-
-
-
+//     })
 
--- services/productservices.js
+++ services/productservices.js
@@ -1,59 +1,129 @@
 const ProductModel =require('../models/productmodel') ;
-const slugify = require('slugify')
-const asyncHandler = require('express-async-handler')
-const ApiError =require('../utils/ApiErrors')
+const asyncHandler = require('express-async-handler');
+const factory = require('./handlersFactory');
+const { v4: uuidv4 } = require('uuid');
+const sharp = require('sharp');
+const { uploadMixOfImages } = require('../middlewares/uploadImageMiddleware');
 
 
-// get all product
-exports.getProducts = asyncHandler(async (req,res) => {
-    const page =req.query.page * 1 || 1 ;
-    const limit =req.query.limit * 1 || 5 ;
-    const skip =(page-1)*limit;
-    const products = await ProductModel.find({}).skip(skip).limit(limit) ;
-    res.status(201).json({ result : products.length , data:products})  
-    })
-
-
-
-
-    // get method to get product by id (specific category)
-exports.getProduct=asyncHandler( async (req , res ,next) => {
-    const { id } = req.params ;
-    const product =await ProductModel.findById(id);
-    if(!product){
-     return next(new ApiError(`No product By This Id ${id}`,404))
+exports.uploadProductImages = uploadMixOfImages([
+    {
+      name: 'imageCover',
+      maxCount: 1,
+    },
+    {
+      name: 'images',
+      maxCount: 5,
+    },
+  ]);
+  
+  exports.resizeProductImages = asyncHandler(async (req, res, next) => {
+    // console.log(req.files);
+    //1- Image processing for imageCover
+    if (req.files.imageCover) {
+      const imageCoverFileName = `product-${uuidv4()}-${Date.now()}-cover.jpeg`;
+  
+      await sharp(req.files.imageCover[0].buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 95 })
+        .toFile(`uploads/products/${imageCoverFileName}`);
+  
+      // Save image into our db
+      req.body.imageCover = imageCoverFileName;
     }
-    res.status(201).json({data:product}) ;
-    })
+    //2- Image processing for images
+    if (req.files.images) {
+      req.body.images = [];
+      await Promise.all(
+        req.files.images.map(async (img, index) => {
+          const imageName = `product-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
+  
+          await sharp(img.buffer)
+            .resize(2000, 1333)
+            .toFormat('jpeg')
+            .jpeg({ quality: 95 })
+            .toFile(`uploads/products/${imageName}`);
+  
+          // Save image into our db
+          req.body.images.push(imageName);
+        })
+      );
+  
+      next();
+    }
+  });
+
+
+// @desc    Get list of products
+// @route   GET /api/v1/products
+// @access  Public
+exports.getProducts = factory.getAll(ProductModel, 'Products');
+// exports.getProducts = asyncHandler(async (req,res) => {
+//     const page =req.query.page * 1 || 1 ;
+//     const limit =req.query.limit * 1 || 5 ;
+//     const skip =(page-1)*limit;
+//     const products = await ProductModel.find({}).skip(skip).limit(limit) ;
+//     res.status(201).json({ result : products.length , data:products})  
+//     })
 
 
 
-//method to create of product(only admin)
-exports.createProduct =asyncHandler(  async (req ,res) => {
-    const product =await ProductModel.create(req.body) ;
-    res.status(201).json({data : product}) ;
-        } )
+// @desc    Get specific product by id
+// @route   GET /api/v1/products/:id
+// @access  Public
+exports.getProduct = factory.getOne(ProductModel, 'reviews');
+// exports.getProduct=asyncHandler( async (req , res ,next) => {
+//     const { id } = req.params ;
+//     const product =await ProductModel.findById(id);
+//     if(!product){
+//      return next(new ApiError(`No product By This Id ${id}`,404))
+//     }
+//     res.status(201).json({data:product}) ;
+//     })
 
-//method to update product (only admin)
-exports.updateProduct= asyncHandler( async(req,res) => {
-    const { id } =req.params ;
-    const product =await ProductModel.findByIdAndUpdate({_id:id},req.body,{new:true})
-    if(!product){
-        return next(new ApiError(`No product By This Id ${id}`,404))  
-        }
-        res.status(201).json({data:product})
+
+
+// @desc    Create product
+// @route   POST  /api/v1/products
+// @access  Private
+exports.createProduct = factory.createOne(ProductModel);
+// exports.createProduct =asyncHandler(  async (req ,res) => {
+//     const product =await ProductModel.create(req.body) ;
+//     res.status(201).json({data : product}) ;
+//         } )
+
+
+
+
+// @desc    Update specific product
+// @route   PUT /api/v1/products/:id
+// @access  Private
+exports.updateProduct = factory.updateOne(ProductModel);
+// exports.updateProduct= asyncHandler( async(req,res) => {
+//     const { id } =req.params ;
+//     const product =await ProductModel.findByIdAndUpdate({_id:id},req.body,{new:true})
+//     if(!product){
+//         return next(new ApiError(`No product By This Id ${id}`,404))  
+//         }
+//         res.status(201).json({data:product})
     
-    })
+//     })
 
-    //method to delete product (only admin)
-exports.deleteproduct=asyncHandler( async(req,res) => {
-    const { id } =req.params ;
-    const product =await ProductModel.findByIdAndDelete(id);
-    if(!product){
-        return next(new ApiError(`No product By This Id ${id}`,404))  
-        }
-        res.status(204).json()
+
+
+// @desc    Delete specific product
+// @route   DELETE /api/v1/products/:id
+// @access  Private
+exports.deleteproduct = factory.deleteOne(ProductModel);
+// exports.deleteproduct=asyncHandler( async(req,res) => {
+//     const { id } =req.params ;
+//     const product =await ProductModel.findByIdAndDelete(id);
+//     if(!product){
+//         return next(new ApiError(`No product By This Id ${id}`,404))  
+//         }
+//         res.status(204).json()
     
-    })
+//     })
 
 
